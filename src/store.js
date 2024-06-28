d398fdc5c3a455a7532dd199d5925ef7f59cbc5b
jdromero88 <jdromerov88@gmail.com>
2024-06-28 11:05:02 -0400

    Add ISP as an option in the select component

--- src/store.js
+++ src/store.js
@@ -8,6 +8,7 @@ export const currentYear = readable(new Date().getFullYear())
 export const yearShowing = writable(2024) // todo: find a way to get the current year
 export const years = readable([2024, 2023])
 export const ispSubPrograms = readable([
+  'International Security Program',
   'Aerospace Security Project',
   'Arleigh A. Burke Chair in Strategy',
   // 'Defending Democratic Institutions',
