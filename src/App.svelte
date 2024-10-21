60201e059366acbedd36273076567219b170939d
jdromero88 <jdromerov88@gmail.com>
2024-10-21 14:21:58 -0400

    Title is created dynamically, packages updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { yearShowing } from './store'
+  import { yearShowing, titleValue, user } from './store'
   import { onMount } from 'svelte'
   import { getNewData } from './data'
   import MainContainer from './components/MainContainer.svelte'
@@ -15,6 +15,10 @@
     dataset = await getNewData()
   }
 
+  // This will run on component mount and update when the store changes
+  $: document.title = $user
+    ? `Metrics for ${$user} | CSIS External Relations`
+    : $titleValue
   onMount(fetchData)
 </script>
 
